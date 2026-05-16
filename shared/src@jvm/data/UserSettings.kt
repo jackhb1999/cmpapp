ee6ca7c81a48dfbdc17a3932376c04796d8da184
fanjx <jack.h.b.fan@outlook.com>
2026-05-16 22:00:10 +0800

    静态文件不稳定，过滤提交

---
+++ shared/src@jvm/data/UserSettings.kt
@@ -0,0 +1,37 @@
+package data
+
+import androidx.datastore.core.Serializer
+import model.UserSettingsData
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.Json
+import java.io.InputStream
+import java.io.OutputStream
+import kotlin.text.decodeToString
+import kotlin.text.encodeToByteArray
+
+
+object UserSettingsSerializer : Serializer<UserSettingsData> {
+    override val defaultValue: UserSettingsData
+        get() = UserSettingsData()
+
+    override suspend fun readFrom(input: InputStream): UserSettingsData {
+        return try {
+            Json.decodeFromString(
+                deserializer = UserSettingsData.serializer(),
+                string = input.readBytes().decodeToString(),
+            )
+        } catch (e: SerializationException) {
+            defaultValue
+        }
+    }
+
+    override suspend fun writeTo(t: UserSettingsData, output: OutputStream) {
+        output.write(
+            Json.encodeToString(
+                serializer = UserSettingsData.serializer(),
+                value = t,
+            ).encodeToByteArray()
+        )
+    }
+
+}
